4d9b80c140c298a7a65dd2051a1cc2447dd72033
Cameron <cameronjmaccabe@gmail.com>
2019-04-02 22:10:25 -0700

    created JSON data storage location

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+let friends = [
+  {
+    "name": "Cameron",
+    "photo": "https://cdn.bandmix.com/bandmix_us/media/1254/1254271/1523747-p.jpg",
+    "scores": [
+      5,
+      2,
+      2,
+      5,
+      5,
+      5,
+      5,
+      4,
+      4,
+      3
+    ]
+  }
+];
+
+module.exports = friends;
